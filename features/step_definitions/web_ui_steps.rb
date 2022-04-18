3762a1e5888c3585e3439b4b4798e1e1bfdefab2
avannim <85245343+avannim@users.noreply.github.com>
2022-04-18 09:13:59 +0300

    Update web_ui_steps.rb

--- features/step_definitions/web_ui_steps.rb
+++ features/step_definitions/web_ui_steps.rb
@@ -25,3 +25,37 @@ When(/^я должен увидеть текст на странице "([^"]*)"
   sleep 1
   expect(page).to have_text text_page
 end
+
+When(/^кликаю по кнопке с адресом "(.+?)"$/) do |url|
+  link_first = find("//a[@href='#{url}/']/div")
+  link_first.click
+  $logger.info("Переход на страницу #{url} осуществлен")
+  sleep 1
+end
+
+When(/^кликаю по первой ссылке содержащей "(.+?)"$/) do |url|
+  link_first = find("//a[contains(@href,'#{url}/'][1]/div")
+  link_first.click
+  $logger.info("Загрузка файла начата")
+  sleep 1
+end
+
+When(/^проверяю наличие файла "(.+?)"$/) do |name|
+  if File.exist?(name)
+    $logger.info("Файл загружен")
+  else
+    $logger.info("Файл не загружен")
+  end
+  sleep 1
+end
+
+When(/^проверяю соответствие имени скачанного файла "(.+?)" с именем файла по ссылке "(.+?)" на сайте$/) do |name, site_name|
+  file_name = name
+  sname = File.readlink(site_name)
+  if file_name == sname
+    $logger.info("Загружена стабильная версия")
+  else
+    $logger.info("Загружена не верный файл")
+  end
+  sleep 1
+end
