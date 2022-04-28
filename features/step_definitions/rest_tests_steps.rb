78fecf2767c6c6e0931c43b6b8f1951ce3dd633e
avannim <85245343+avannim@users.noreply.github.com>
2022-04-28 11:46:56 +0300

    Update rest_tests_steps.rb

--- features/step_definitions/rest_tests_steps.rb
+++ features/step_definitions/rest_tests_steps.rb
@@ -57,33 +57,51 @@ When(/^нахожу пользователя с логином (\w+\.\w+)$/) do 
 end
 
 When(/^удаляю пользователя c логином (\w+\.\w+)$/) do |login|
-  response = $rest_wrap.delete('/users', login: login)
+  if @scenario_data.users_id[login].nil?
+    @scenario_data.users_id[login] = find_user_id(users_information: @scenario_data
+                                                                       .users_full_info,
+                                                  user_login: login)
+  end
+  id=@scenario_data.users_id[login]
+  response = $rest_wrap.delete('/users/'+"#{id}")
   $logger.info(response.inspect)
 end
 
-When(/^пользователю c логином (\w+\.\w+) передаю имя (\w+) фамилию (\w+) пароль ([\d\w@!#]+)$/) do
-|login, name, surname, password|
-
-  response = $rest_wrap.put('/users', login: login,
-                             name: name,
+When(/^пользователю c логином (\w+\.\w+) передаю имя (\w+) фамилию (\w+)$/) do
+|login, name, surname|
+  if @scenario_data.users_id[login].nil?
+    @scenario_data.users_id[login] = find_user_id(users_information: @scenario_data
+                                                                       .users_full_info,
+                                                  user_login: login)
+  end
+  id=@scenario_data.users_id[login]
+  response = $rest_wrap.put('/users/'+"#{id}", name: name,
                              surname: surname,
-                             password: password,
                              active: 1)
   $logger.info(response.inspect)
 end
 
-When(/^проверяю (наличие|отсутствие) логина (\w+\.\w+) с именем (\w+) фамилией (\w+) паролем ([\d\w@!#]+)$/) do |presence, login, name, surname, password|
-  search_login_in_list = true
-  presence == 'отсутствие' ? search_login_in_list = !search_login_in_list : search_login_in_list
-
-  logins_from_site = @scenario_data.users_full_info.map { |f| f.try(:[], 'login') }
-  login_presents = logins_from_site.include?(login)
-
-  if login_presents
-    message = "Логин #{login} с именем #{name} фамилией #{surname} паролем #{password} присутствует в списке пользователей"
-    search_login_in_list ? $logger.info(message) : raise(message)
-  else
-    message = "Логин #{login} отсутствует в списке пользователей"
-    search_login_in_list ? raise(message) : $logger.info(message)
+When(/^нахожу пользователя с логином (\w+\.\w+) с именем (\w+) фамилией (\w+)$/) do |login, name, surname|
+  login_id = @scenario_data.users_id[login].nil?
+  if @scenario_data.users_id[login].nil?
+    @scenario_data.users_id[login] = find_user_id(users_information: @scenario_data
+                                                                       .users_full_info,
+                                                  user_login: login)
   end
+
+  users_name = 0
+  users_surname = 0
+  @scenario_data.users_full_info.each do |user|
+    next unless user['login'] == login
+    if user['login'] == login
+      users_name = user['name']
+      users_surname = user['surname']
+    end
+  end
+
+  if login_id == false && users_name == name && users_surname == surname
+    $logger.info("Логин #{login} с именем #{name} фамилией #{surname} присутствует в списке пользователей")
+    else
+      $logger.info("Логин #{login} с именем #{name} фамилией #{surname} отсутствует в списке пользователей")
+    end
 end
